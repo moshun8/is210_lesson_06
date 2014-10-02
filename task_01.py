22a0b2b41531c93b514f16ad0294a10746781426
Maureen Kilmade <maureen.kilmade@spsmail.cuny.edu>
2014-10-02 10:51:14 -0400

    Evens & Odds, syntax problems I think

---
+++ task_01.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Evens and Odds"""
+
+
+def evens_and_odds(numbers=[], show_even=True):
+    evens = []
+    odds = []
+    for num in numbers:
+        if num % 2 == 0:
+             num is "even"
+        else:
+             num is "odd"
+             odds.append(num)
+    while show_even:
+        if num is "even":
+             evens.append(num)
+        else:
+             odds.append(num)
