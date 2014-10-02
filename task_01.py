5449402d1730703543f46810c02bf01a19457dc6
Maureen Kilmade <maureen.kilmade@spsmail.cuny.edu>
2014-10-02 14:01:39 -0400

    O's and 0's look very similar

--- task_01.py
+++ task_01.py
@@ -8,7 +8,7 @@ import data
 
 def evens_and_odds(numbers=[], show_even=True):
     """Finds even and odd numbers, adds to list"""
-    for num in data.TASK_01:
+    for num in data.TASK_O1:
         if num % 2 == 0:
             if show_even is True:
                 numbers.append(num)
