079f6b9a32a20b70eef176a9a0e5bd42fdd8e824
Maureen Kilmade <maureen.kilmade@spsmail.cuny.edu>
2014-10-02 13:39:45 -0400

    pylint errors, dont know why it won't import data.TASK_01

--- task_01.py
+++ task_01.py
@@ -5,12 +5,13 @@
 
 import data
 
+
 def evens_and_odds(numbers=[], show_even=True):
     """Finds even and odd numbers, adds to list"""
     for num in data.TASK_01:
         if num % 2 == 0:
-            if show_even == True:
-                evens.append(num)
+            if show_even is True:
+                numbers.append(num)
         else:
-            if show_even == False:
-                odds.append(num)
+            if show_even is False:
+                numbers.append(num)
