ef9bb6b8183cc529d3f5ffa134e4ab5a01b285bf
Maureen Kilmade <maureen.kilmade@spsmail.cuny.edu>
2014-10-05 16:15:57 -0400

    changed so task 3 can handle None

--- task_03.py
+++ task_03.py
@@ -8,11 +8,18 @@ import data
 
 def bubble_sort(lst=[]):
     """Puts numbers in order 1 by 1"""
-    length = len(lst)
-    for numbers in range(length):
-        for numbers in range(length - 1):
-            if lst[numbers] > lst[numbers + 1]:
-                lst[numbers], lst[numbers + 1] = lst[numbers + 1], lst[numbers]
+    if isinstance(lst, list):
+        length = len(lst) - 1
+        sorted = False
+        while not sorted:
+            sorted = True
+            for numbers in range(length):
+                if lst[numbers] > lst[numbers + 1]:
+                    sorted = False
+                    lst[numbers], lst[numbers + 1] = lst[numbers + 1], lst[numbers]
+    else:
+        lst = []
+    return lst
 
 bubble_sort(data.TASK_O1)
 print data.TASK_O1
