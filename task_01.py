ba6144390fb71460d2509dd2fefd86410de560a9
Maureen Kilmade <maureen.kilmade@spsmail.cuny.edu>
2014-10-05 08:12:43 -0400

    fixed infinite loop

--- task_01.py
+++ task_01.py
@@ -8,10 +8,12 @@ import data
 
 def evens_and_odds(numbers=[], show_even=True):
     """Finds even and odd numbers, adds to list"""
-    for num in data.TASK_O1:
+    odd_or_even = []
+    for num in numbers:
         if num % 2 == 0:
             if show_even is True:
-                numbers.append(num)
+                odd_or_even.append(num)
         else:
             if show_even is False:
-                numbers.append(num)
+                odd_or_even.append(num)
+    return odd_or_even
